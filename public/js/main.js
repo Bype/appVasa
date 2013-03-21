9af0d35d40165889f9514ad7a66833b8949460d8
David Olivari <david.olivari@gmail.com>
2013-03-21 10:32:06 +0100

    get rid of backbone in this template

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,16 @@
 requirejs.config({
 	paths : {
 		underscore : 'lib/underscore',
-		backbone : 'lib/backbone',
 		bootstrap : 'lib/bootstrap'
 	},
 	shim : {
-		'backbone' : {
-			//These script dependencies should be loaded before loading
-			//backbone.js
-			deps : ['underscore', 'jquery'],
-			//Once loaded, use the global 'Backbone' as the
-			//module value.
-			exports : 'Backbone'
-		},
 		'underscore' : {
 			exports : '_'
 		}
 	}
 });
 
-require(['backbone','bootstrap'], function(Backbone) {
+require(['jquery', 'underscore', 'bootstrap'], function($, _) {
 	$(function() {
 
 	});
